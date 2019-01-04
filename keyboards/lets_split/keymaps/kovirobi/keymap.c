7e5d899511714f6491cf35598dfddd7f1b17106b
Kovacsics Robert (NixOS) <kovirobi@gmail.com>
2019-01-04 00:37:29 +0000

    KoviRobi Steno keymap update (Direction keys)

--- keyboards/lets_split/keymaps/kovirobi/keymap.c
+++ keyboards/lets_split/keymaps/kovirobi/keymap.c
@@ -30,6 +30,7 @@ enum custom_keycodes {
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+#define NKRO    MAGIC_TOGGLE_NKRO
 
 #define LS(C) MT(MOD_LSFT, KC_##C)
 #define LC(C) MT(MOD_LCTL, KC_##C)
@@ -45,20 +46,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |  \   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Adjust| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |Adjust| Ctrl | Alt  | GUI  |Lower |Space | Tab  |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_BSPC, \
+   KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_MINS, \
    KC_ESC,  LS(A),   LS(S),   LC(D),   LC(F),   KC_G,     KC_H,    RC(J),   RC(K),    RS(L),   RS(SCLN), KC_QUOT, \
    KC_LSFT, LG(Z),   LG(X),   LA(C),   LA(V),   KC_B,     KC_N,    RA(M),   RA(COMM), RG(DOT), RG(SLSH), KC_RSFT, \
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LO(SPC), RZ(BSPC), LO(TAB), RZ(ENT), KC_MPLY,  ROT_A,   ROT_B,    KC_D     \
+   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LO(SPC), RZ(BSPC), LO(TAB), RZ(ENT), KC_MPLY,  ROT_A,   ROT_B,    XXXXXXX  \
 ),
 
 /* Plover layer (http://opensteno.org)
@@ -74,15 +75,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_PLOVER] = LAYOUT_ortho_4x12( \
-  STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC, \
-  STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR, \
-  EXT_PLV, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, STN_A,   STN_O,   STN_E,   STN_U,   KC_MPLY, ROT_A,   ROT_B,   KC_D    \
+  STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5, STN_N6,  STN_N7,  STN_N8, STN_N9,  STN_NA, STN_NB, STN_NC, \
+  STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL, STN_ST1, STN_ST3, STN_FR, STN_PR,  STN_LR, STN_TR, STN_DR, \
+  EXT_PLV, STN_S2,  STN_KL,  STN_WL,  STN_RL, STN_ST2, STN_ST4, STN_RR, STN_BR,  STN_GR, STN_SR, STN_ZR, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, STN_A,  STN_O,   STN_E,   STN_U,  KC_MPLY, ROT_A,  ROT_B,  XXXXXXX \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -92,15 +93,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,    KC_LPRN, KC_RPRN, _______, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS), S(KC_NUBS), _______, _______, _______, \
-  _______, _______, _______, _______, UNLOWER, RAISE,   UNLOWER, RAISE,    KC_MSTP,    ROT_A,   ROT_B,   KC_D    \
+  _______, _______, _______, _______, UNLOWER, RAISE,   UNLOWER, RAISE,    KC_MSTP,    ROT_A,   ROT_B,   _______  \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -110,28 +111,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_0,    KC_6,    KC_7,    KC_8,    KC_9,    KC_DEL, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_0,    KC_6,    KC_7,    KC_8,    KC_9,    _______, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, LOWER,   UNRAISE, LOWER,   UNRAISE, KC_MSEL, ROT_A,   ROT_B,   KC_D    \
+  _______, _______, _______, _______, LOWER,   UNRAISE, LOWER,   UNRAISE, KC_MSEL, ROT_A,   ROT_B,   _______  \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * | NKRO |      |      |      |      |      |      |      |      |      |      | NKRO |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |Plover|      | Home | PgUp | PgDn | End  | Left | Down |  Up  |Right |      |Plover|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |Reset |      |      |      |      |      |      |      |      |      |      |Reset |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______, MAGIC_TOGGLE_NKRO, _______, _______, _______, _______, _______, PLOVER,  _______, _______, MAGIC_TOGGLE_NKRO, KC_DEL,  \
-  _______, RESET,             _______, _______, _______, _______, _______, PLOVER,  _______, _______, RESET,             _______, \
-  _______, RESET,             _______, _______, _______, _______, _______, PLOVER,  _______, _______, RESET,             _______, \
-  _______, _______,           _______, _______, _______, _______, _______, _______, KC_EJCT, ROT_A,   ROT_B,             KC_D     \
+  NKRO,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, NKRO,    \
+  PLOVER,  _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, PLOVER, \
+  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_EJCT, ROT_A,   ROT_B,   _______  \
 )
 
 
